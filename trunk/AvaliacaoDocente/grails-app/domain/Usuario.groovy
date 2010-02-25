7f43f69a52b13a09b4f1920765fc5472a0f37b9a
pepetofagner@gmail.com <pepetofagner@gmail.com@d959a1f6-216e-11df-b401-3b86f75e9ead>
2010-02-25 23:12:57 +0000

    teste

--- trunk/AvaliacaoDocente/grails-app/domain/Usuario.groovy
+++ trunk/AvaliacaoDocente/grails-app/domain/Usuario.groovy
@@ -4,7 +4,6 @@
 class Usuario {
     static transients = ['pass']
     static hasMany = [authorities: Permissao, disciplina: Disciplina]
-
     static belongsTo = [Permissao, Disciplina]
 
     String username
