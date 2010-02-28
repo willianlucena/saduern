d0bf2dec90c52fc02ca5084949ddd034b8ef0cf4
maxwellpaivaa@gmail.com <maxwellpaivaa@gmail.com@d959a1f6-216e-11df-b401-3b86f75e9ead>
2010-02-28 19:42:35 +0000

    Tela De login do SADUERN

--- trunk/AvaliacaoDocente/grails-app/conf/UrlMappings.groovy
+++ trunk/AvaliacaoDocente/grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,15 @@
 class UrlMappings {
-    static mappings = {
-      "/$controller/$action?/$id?"{
-	      constraints {
-			 // apply constraints here
-		  }
-	  }
-      "/"(view:"/index")
-	  "500"(view:'/error')
-	}
+  static mappings = {
+    "/$controller/$action?/$id?"{
+      constraints {
+        // apply constraints here
+      }
+    }
+    "500"(view:'/error')
+    "/" {
+      controller = "login"
+      action = "auth"
+    }
+  }
 }
+
